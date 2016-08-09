5975bf23bde6587d3b43f1a9e2609df1a59d376a
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2016-08-09 12:49:38 +0200

    Force "main environments" deploy
    r10k takes git branches in alphabetical order, it's possible
    production envs are in the end of the list, hence it might
    take a looong time before a PM is "really" up.
    
     Deploying first a subset of "main environments", then "the rest"
    should help a bit.

--- docker-entrypoint.d/05-initialize-code.sh
+++ docker-entrypoint.d/05-initialize-code.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-r10k deploy environment -pv
+if [ -n $MAIN_ENVIRONMENTS ]; then
+  for env in $(echo $MAIN_ENVIRONMENTS | tr ',' "\n"); do
+    echo r10k deploy environment $env -pv
+  done
+fi
+echo r10k deploy environment -pv
